4e7c34b51d3c9fddfafbf2e93818e65545668762
BIAsia <980492289@qq.com>
2022-08-19 20:21:12 +0800

    add links to menu

--- page/home.js
+++ page/home.js
@@ -18,19 +18,18 @@ document.querySelector("#home").innerHTML = `
 </div>
 <div class="col-container">
     <div class="col col-main section-list menu">
-        <div class="section section-m menu__item">
+        <a class="section section-m menu__item" target="_blank" href="https://biasia.notion.site/UI-UX-Design-768dba6b539e4289a4ee0eede4eb3d5b">
             <h3 class="menu__item-textinner">UI/UX Designer</h3>
             <p>worked at Sensetime/Tencent ISUX/Mobiuspace</p>
-        </div>
-        <div class="section section-m menu__item">
+        </a>
+        <a class="section section-m menu__item" target="_blank" href="https://biasia.notion.site/Web-Design-Develop-2ea579e464a54b4eb82f06b5f8e51416">
             <h3 class="menu__item-textinner">Web Designer & Developer</h3>
             <p>with Framer & Three.js</p>
-        </div>
-        <div class="section section-m menu__item">
+        </a>
+        <a class="section section-m menu__item" target="_blank" href="https://biasia.notion.site/Plugin-Develop-8d9bb04f3d78452a94b3bceebf4c1059">
             <h3 class="menu__item-textinner">Plugin Developer</h3>
             <p>for Figma & Adobe XD</p>
-            
-        </div>
+        </a>
     </div>
     <div class="col">
         <div class="comment">I AM ALSO A...</div>
